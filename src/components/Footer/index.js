0fc62073ba8f62bcd71990309713aa973e59a393
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-06-29 12:57:27 +0700

    feat(component): create Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <div className="container">
+            <div className="footer clearfix mb-0 text-muted">
+                <div className="float-start">
+                    <p>2023 &copy; Naufal Fadhil Athallah</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+  )
+}
